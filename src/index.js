81dc652be2e40e96e251e90874f0a3eb26267d02
dy0917 <huangkingsley@gmail.com>
2024-12-09 18:25:57 +1300

    add dotenv lib

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 8080;
 
+console.log("process.env.HOST_ENV", process.env.SECRET);
 app.get("/", (req, res) => {
-  res.send("Hello Kingsley! module 10");
+  res.send(`Hello Kingsley! module 10 ${process.env.HOST_ENV} this is my secret value: 
+     ${process.env.SECRET}`);
 });
 
 app.listen(port, () => {
